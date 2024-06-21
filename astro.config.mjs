9836d49b95f8eaeb3e11ca6c5d714d4eb4869735
Jonathan Alexis Bello Lopez <jonathan.bellofg@gmail.com>
2024-06-20 21:55:11 -0600

    add: Carousel main component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, squooshImageService } from "astro/config";
 import react from "@astrojs/react";
 
 import tailwind from "@astrojs/tailwind";
@@ -7,4 +7,7 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [react(), tailwind()],
   output: "static",
+  image: {
+    service: squooshImageService(),
+  },
 });
